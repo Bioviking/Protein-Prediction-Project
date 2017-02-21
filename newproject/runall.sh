54e1e1309306cb1917e915e44be842e83da50556
Ryno Lawson <unleashedracing@gmail.com>
2017-02-21 15:32:58 +0100

    Inserting initial python files and updating runall.sh for automating the push to repo

---
+++ newproject/runall.sh
@@ -0,0 +1,11 @@
+
+#Automating my add, commit and push command
+#Parameters message
+#automate username and pword
+
+git add .
+git status
+git commit -m "Inserting dataset and project plan starting layout"
+git push origin master 
+
+#username and pword
